ba2e09f329cd7245eb84e183f7c566b62d1aa5c7
mbuschmann <github@matthias-buschmann.org>
2022-11-04 13:04:37 +0100

    updating plots

--- 125HR_preview.py
+++ 125HR_preview.py
@@ -397,19 +397,19 @@ class Preview125(QtWidgets.QMainWindow):
         # update plots
         y = np.abs(self.spc[10:int(len(self.spc)/2)])
         self._line1.set_data((self.wvn[10:], y))
-        if self.run == 2:
-            self._dynamic_ax2.set_xlim(self.config['spc_plot_xlim'])
-            self._dynamic_ax2.set_ylim(np.min(y)*1.2, np.max(y)*1.2)
+        #if self.run == 2:
+        #    self._dynamic_ax2.set_xlim(self.config['spc_plot_xlim'])
+        #    self._dynamic_ax2.set_ylim(np.min(y)*1.2, np.max(y)*1.2)
         self._line1.figure.canvas.draw()
         self._line2.set_data((np.arange(len(self.preview.ifg)), self.preview.ifg))
-        if self.run == 2:
-            #self._dynamic_ax2.set_xlim(0,4000)
-            self._dynamic_ax2.set_ylim(np.min(self.preview.ifg)*1.2, np.max(self.preview.ifg)*1.2)
+        #if self.run == 2:
+        #    #self._dynamic_ax2.set_xlim(0,4000)
+        #    self._dynamic_ax2.set_ylim(np.min(self.preview.ifg)*1.2, np.max(self.preview.ifg)*1.2)
         self._line2.figure.canvas.draw()
-        self._line3.set_data((np.arange(len(self.preview.ifg)), self.ifg_s))
-        if self.run == 2:
-            #self._dynamic_ax2.set_xlim(0,4000)
-            self._dynamic_ax3.set_ylim(np.min(self.ifg_s)*1.2, np.max(self.ifg_s)*1.2)
+        self._line3.set_data((np.arange(len(self.preview.ifg)), self.ifg_s/np.max(self.ifg_s)))
+        #if self.run == 2:
+        #    #self._dynamic_ax2.set_xlim(0,4000)
+        #    self._dynamic_ax3.set_ylim(np.min(self.ifg_s)*1.2, np.max(self.ifg_s)*1.2)
         self._line3.figure.canvas.draw()
         
         
@@ -446,11 +446,11 @@ class Preview125(QtWidgets.QMainWindow):
         #self._dynamic_ax1.set_ylim(0,1)
         self._line1, = self._dynamic_ax1.plot(np.linspace(3000, 11000, 4000), np.ones(4000), 'b-')
         self._dynamic_ax2 = dynamic_canvas2.figure.subplots()
-        self._dynamic_ax3 = self._dynamic_ax2.twinx()
+        #self._dynamic_ax3 = self._dynamic_ax2.twinx()
         #self._dynamic_ax2.set_xlim(0,4000)
         #self._dynamic_ax2.set_ylim(-1,1)
         self._line2, = self._dynamic_ax2.plot(np.arange(4000), np.ones(4000), '-', color='gray')
-        self._line3, = self._dynamic_ax3.plot(np.arange(4000), np.ones(4000), 'k-')
+        self._line3, = self._dynamic_ax2.plot(np.arange(4000), np.ones(4000), 'k-')
         # Setup timer to repeat measurement cycle
         self._timer1 = dynamic_canvas1.new_timer(self.config['refreshrate']*1000)
         self._timer1.add_callback(self._update)
